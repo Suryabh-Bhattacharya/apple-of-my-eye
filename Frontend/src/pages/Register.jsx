6ad6bf8f4e69beadc00069a2d748ec56ec574656
Rohit2853 <105515073+Rohit2853@users.noreply.github.com>
2024-02-24 11:11:58 +0530

    Updated Register.jsx with otp features

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -9,8 +9,11 @@ function Register() {
   const navigate = useNavigate();
 
   const [rollNo, setRollNo] = useState('');
-  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
   const [password, setPassword] = useState('');
+  const [otp, setOpt] = useState('');
+  const [optsent, setOptsent] = useState(0);
+  
   async function registerUser(event){
     event.preventDefault();
     const response = await fetch('http://localhost:5000/api/register',{
@@ -20,17 +23,42 @@ function Register() {
       },
       body: JSON.stringify({
         rollNo,
-        email,
+        phoneNumber,
         password,
+        otp
       })
     })
 
     const data = await response.json();
     if(data.status === 'ok'){
-      navigate('/login',{replace:true});
+      navigate('/dashboard',{replace:true});
     }
+    setOptsent(1);
     // console.log(data);
   }
+
+  async function getOtp(){
+    // event.preventDefault();
+    const response = await fetch('http://localhost:5000/api/getOtp',{
+      method: 'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify({
+        rollNo,
+        phoneNumber,
+        password,
+      })
+    });
+    const data = await response.json();
+    if(data.status === 'error'){
+      navigate('/register',{replace:true});
+    }
+    // setOpt(data.otp);
+    setOptsent(1);
+
+  }
+
   return (
     <div>
       <Form onSubmit={registerUser} className='loginform'>
@@ -42,32 +70,45 @@ function Register() {
           value={rollNo}
           onChange={(e)=>setRollNo(e.target.value)}/>
         </Form.Group>
-
-
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
+        <Form.Group className="mb-3" controlId="">
+          <Form.Label>Phone Number</Form.Label>
           <Form.Control 
-          type="email" 
-          placeholder="Enter email" 
-          value={email}
-          onChange={(e)=>setEmail(e.target.value)}
+          type="number" 
+          placeholder="Phone Number" 
+          value={phoneNumber}
+          onChange={(e)=>setPhoneNumber(e.target.value)}
           />
         </Form.Group>
-
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control 
           type="password" 
           placeholder="Password" 
           value={password}
-          onChange={(e)=>setPassword(e.target.value)} 
-          />
+          onChange={(e)=>setPassword(e.target.value)} />
         </Form.Group>
-        <Button variant="primary" 
-        type="submit"
-        value="Register">
-          Register
-        </Button>
+        {!optsent? (
+          <Button variant="primary" onClick={getOtp}>
+            Get Otp
+          </Button>
+        ):(
+          <div>
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>OTP</Form.Label>
+            <Form.Control 
+            type="number" 
+            placeholder="OTP" 
+            value={otp}
+            onChange={(e)=>setOpt(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" 
+          type="submit"
+          value="Register">
+            Register
+          </Button>
+          </div>
+        )}
       </Form>
     </div>
   )
