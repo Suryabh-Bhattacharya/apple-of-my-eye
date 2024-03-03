22d5c2cc2801e3d269adac14e4a11fcce794169e
Rohit2853 <105515073+Rohit2853@users.noreply.github.com>
2024-03-03 12:26:56 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -2,15 +2,27 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
 import "bootstrap/dist/css/bootstrap.min.css"
 import './Login.css'
 
 export default function Login() {
   
-  const [email, setEmail] = useState('');
+  const [rollNo, setRollno] = useState('');
   const [password, setPassword] = useState('');
+  //used for alert message
+  const [show, setShow] = useState(true);
+  const [err,setErr] = useState(false);
+  const [alerttype,setAlerttype] = useState('');
+  const [alertmsg,setAlertmsg] = useState('');
   const navigate = useNavigate();
-
+  
+  const  handleClose =()=>{
+    setShow(false);
+    setErr(0);
+    setAlerttype('');
+    setAlertmsg('');
+  }
   async function loginUser(event){
     event.preventDefault();
     const response = await fetch('http://localhost:5000/api/login',{
@@ -19,28 +31,51 @@ export default function Login() {
         'Content-Type':'application/json',
       },
       body: JSON.stringify({
-        email,
+        rollNo,
         password,
       })
     })
-
+    console.log("api called")
     const data = await response.json();
+
     if(data.status === 'ok'){
+      console.log(data.authtoken);
+      localStorage.setItem('token',JSON.stringify(data.authtoken));
+      // localStorage.setItem('user',JSON.stringify(data.id));
       navigate('/dashboard',{replace:true})
+    }
+    else if(data.status === 'wrongPassword'){
+      setAlerttype("danger");
+      setAlertmsg(data.msg);
+      setErr(1);
+    }
+    else{
+      setAlerttype("danger");
+      setAlertmsg("User Not Registered");
+      setErr(1);
     }
     console.log(data);
   }
+
+  function alertFunc(){
+    return (<Alert variant={alerttype} onClose={handleClose}  dismissible>
+      <p>{alertmsg}</p>
+  </Alert>);
+  }
+
   return (
     <div>
       <Form onSubmit={loginUser} className='loginform'>
+      {!err ? (<></>):(alertFunc())}
       <div className='loginTitle'><h1>Login</h1></div>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
+          <Form.Label>Roll No</Form.Label>
           <Form.Control 
-          type="email" 
-          placeholder="Enter email" 
-          value={email}
-          onChange={(e)=>setEmail(e.target.value)}
+          type="rollNo" 
+          placeholder="Roll No" 
+          value={rollNo}
+          onChange={(e)=>setRollno(e.target.value)}
+          required
           />
         </Form.Group>
 
@@ -51,6 +86,7 @@ export default function Login() {
           placeholder="Password" 
           value={password}
           onChange={(e)=>setPassword(e.target.value)} 
+          required
           />
         </Form.Group>
         <Button variant="primary" 
@@ -58,6 +94,8 @@ export default function Login() {
         value="Login">
           Login
         </Button>
+        <br />
+        <p>not yet registered,<a href='/register'>Register Here</a></p>
       </Form>
     </div>
   )
