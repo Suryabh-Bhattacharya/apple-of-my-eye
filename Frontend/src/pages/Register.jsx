c4f9956f995350dc6db769299905118761217aae
Rohit2853 <105515073+Rohit2853@users.noreply.github.com>
2024-03-03 12:25:48 +0530

    Update Register.jsx

--- Frontend/src/pages/Register.jsx
+++ Frontend/src/pages/Register.jsx
@@ -2,6 +2,9 @@ import { useState } from 'react'
 import {useNavigate} from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
+import PhoneInput from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 import "bootstrap/dist/css/bootstrap.min.css"
 import './Login.css'
 
@@ -13,6 +16,22 @@ function Register() {
   const [password, setPassword] = useState('');
   const [otp, setOpt] = useState('');
   const [optsent, setOptsent] = useState(0);
+  const [show, setShow] = useState(true);
+  const [err,setErr] = useState(false);
+  const [alerttype,setAlerttype] = useState('');
+  const [alertmsg,setAlertmsg] = useState('');
+  const  handleClose =()=>{
+    setShow(false);
+    setErr(0);
+    setAlerttype('');
+    setAlertmsg('');
+  }
+
+  function alertFunc(){
+    return (<Alert variant={alerttype} onClose={handleClose} dismissible>
+      <p>{alertmsg}</p>
+  </Alert>);
+  }
   
   async function registerUser(event){
     event.preventDefault();
@@ -33,12 +52,30 @@ function Register() {
     if(data.status === 'ok'){
       navigate('/dashboard',{replace:true});
     }
-    setOptsent(1);
     // console.log(data);
   }
 
   async function getOtp(){
     // event.preventDefault();
+    if (!rollNo || !phoneNumber || !password) {
+      setAlerttype("danger");
+      setAlertmsg("Please fill out all fields.");
+      setErr(true);
+      return;
+    }
+    if(rollNo.length!==9){
+      setAlerttype("danger");
+      setAlertmsg("Enter a valid rollNo");
+      setErr(true);
+      return;
+    }
+    if(phoneNumber.length!==10){
+      setAlerttype("danger");
+      setAlertmsg("Enter a valid 10 digit number");
+      setErr(true);
+      return;
+    }
+    
     const response = await fetch('http://localhost:5000/api/getOtp',{
       method: 'POST',
       headers:{
@@ -52,15 +89,21 @@ function Register() {
     });
     const data = await response.json();
     if(data.status === 'error'){
-      navigate('/register',{replace:true});
+      setAlerttype("danger")
+      setAlertmsg(data.msg);
+      setErr(true);
+      navigate('/login',{replace:true});
     }
-    // setOpt(data.otp);
-    setOptsent(1);
-
+    else{
+      setAlerttype("success")
+      setAlertmsg(data.msg);
+      setOptsent(1);
+    }
   }
 
   return (
     <div>
+      {!err ? (<></>):(alertFunc())}
       <Form onSubmit={registerUser} className='loginform'>
       <div className='loginTitle'><h1>Register</h1></div>
         <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -68,15 +111,18 @@ function Register() {
           <Form.Control type="number" 
           placeholder="Enter RollNo" 
           value={rollNo}
-          onChange={(e)=>setRollNo(e.target.value)}/>
+          onChange={(e)=>setRollNo(e.target.value)}
+          required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="">
           <Form.Label>Phone Number</Form.Label>
           <Form.Control 
-          type="number" 
-          placeholder="Phone Number" 
+          type="tel" 
+          placeholder="without +91" 
           value={phoneNumber}
           onChange={(e)=>setPhoneNumber(e.target.value)}
+          required
           />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -85,7 +131,8 @@ function Register() {
           type="password" 
           placeholder="Password" 
           value={password}
-          onChange={(e)=>setPassword(e.target.value)} />
+          onChange={(e)=>setPassword(e.target.value)} 
+          required/>
         </Form.Group>
         {!optsent? (
           <Button variant="primary" onClick={getOtp}>
@@ -100,6 +147,7 @@ function Register() {
             placeholder="OTP" 
             value={otp}
             onChange={(e)=>setOpt(e.target.value)}
+            required
             />
           </Form.Group>
           <Button variant="primary" 
@@ -109,6 +157,8 @@ function Register() {
           </Button>
           </div>
         )}
+        <br/><br/>
+        <p>Already Registered,<a href='/login'>Login Here</a></p>
       </Form>
     </div>
   )
