acd809e3f9dbc33aacb672badfe82179f9f2e780
Rohit2853 <105515073+Rohit2853@users.noreply.github.com>
2024-03-10 00:22:19 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import react from 'react'
 import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home'
@@ -6,6 +5,7 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import Dashboard from './pages/Dashboard'
 import Pagenotfound from './pages/PagenotFound'
+import Userdata from './pages/Userdata'
 
 function App() {
   return (<div>
@@ -15,6 +15,7 @@ function App() {
         <Route path='/login' exact Component={Login}></Route>
         <Route path='/register' exact Component={Register}></Route>
         <Route path='/dashboard' exact Component={Dashboard}></Route>
+        <Route path='/Userdata' exact Component={Userdata}></Route>
         <Route path='/*' exact Component={Pagenotfound}></Route>
     </Routes>
     </BrowserRouter>
