f836f814566e837897e2966867f390c52d135617
Rohit2853 <105515073+Rohit2853@users.noreply.github.com>
2024-03-10 00:30:01 +0530

    Update Home.jsx

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-// import './Login.css'
+import Button from 'react-bootstrap/Button';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default function Home(){
     return (
-        <div>Welcome to NITT-INE</div>
+        <div className="home">
+            <h1>Welcome to NITT-INE</h1>
+            <Button href="/register">Register</Button>
+            <Button href="/login">Login</Button>
+            <Button href="/Dashboard">Dashboard</Button>
+        </div>
     )
 }
