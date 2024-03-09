81ce053f7c4d9b1a2ee9aea2c5c23d3e3a2f8aa0
Rohit2853 <105515073+Rohit2853@users.noreply.github.com>
2024-03-10 00:35:33 +0530

    Update user.js

--- Backend/Models/user.js
+++ Backend/Models/user.js
@@ -32,15 +32,5 @@ const UserSchema = new Schema({
         default:null,
     }
 })
-//this function will be called before  save has been runned on userschema
-//wont contain doc as it has not been saved yet
-UserSchema.pre('save',async function (next){
-    const salt = await bcrypt.genSalt();
-    this.password = await bcrypt.hash(this.password,salt);
-    next();
-})
-//this function will be called when the save has been runned on userschema
-// UserSchema.post('save',function (doc,next){
-//     next();
-// })
+
 module.exports = mongoose.model('User',UserSchema);
